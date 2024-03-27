4e983ceb605976578bbf33b52482742d5d545641
DanHuntlyCollege <danh@huntlycollege.school.nz>
2024-03-28 09:03:31 +1300

    Update the exercise

--- Function exercise with dictionary passwords exercise.py
+++ Function exercise with dictionary passwords exercise.py
@@ -4,28 +4,21 @@
 # set up a dictionary to hold the data.
 logindict = {'Dan':'123', 'Kiri':'456','Rob':'678','Tama':'234','Kahu':'789'}
 
-# setup a list to hold a user and password pair
-user = ''
-password = ''
-login=[user,password]
-accessallowed = False
-
-# This function should ask the user for a username, and password, and pass those back to the program
-def getlogin():
+# setup a variable to show if the user has been allowed in yet
+accessallowed = False   # this is Boolean and shows if the user is allowed acces yet
 
 
-# this function should receive a username and password pair, in a list (called 'login')
-
-def checklogin(login):
+def checklogin(user,password):
 # Then it needs to say 'Checking login details...'
 
-# next it needs to get the username out of the list (login) that it got handed as an argument, and store it.
-        username = login[0]
+# Lookup the user name and see if they are in the list
+
+# if they are in the list. If they are not, go back to the main program
 
 # next, it needs to through the dictionary called 'logindict', and get the password paired with that username
-        password = logindict[user]
+    
 # next it compares the looked up username to the one from the list it got handed to see if they match 
-        if .........  == login[1]:
+      
             
 # if they do, print 'Access allowed! '
 
@@ -35,5 +28,6 @@ def checklogin(login):
 # Main program - uses a WHILE function to keep going until accessallowed = True
 # program just calls the two funtions to get a login, then check the login
 while accessallowed == False
-    login=getlogin()
-    checklogin(login)
+  # first go and ask the user for a username and password
+  
+    checklogin(user,password) #send the username and passsword to the fuction to look up and see if its correct 
